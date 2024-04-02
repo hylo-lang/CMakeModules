389231691f37beceeee1a972ad0bed326081ce9e
Dave Abrahams <dabrahams@adobe.com>
2024-04-02 15:43:15 -0700

    Patch SwiftNumerics resiliently
    The ExternalProject CMake docs, which cover the PATCH parameter, say:
    
      Note that it can be quite difficult to define an appropriate patch
      command that performs robustly, especially for download methods such
      as git where changing the ``GIT_TAG`` will not discard changes from
      a previous patch, but the patch command will be called again after
      updating to the new tag.
    
    It seems that even not changing the tag will cause the project to be
    updated and re-patched.

--- FindSwiftNumerics.cmake
+++ FindSwiftNumerics.cmake
@@ -1,10 +1,11 @@
 include(./FetchHyloDependency)
 
-set(swift-numerics_patch
-  ${GIT_EXECUTABLE} apply ${CMAKE_CURRENT_LIST_DIR}/patches/swift-numerics.patch)
+set(patch_swift_numerics
+  ${CMAKE_COMMAND} -P ${CMAKE_CURRENT_LIST_DIR}/scripts/GitPatch.cmake
+  ${CMAKE_CURRENT_LIST_DIR}/patches/swift-numerics.patch)
 
 fetch_hylo_dependency(SwiftNumerics
   GIT_REPOSITORY https://github.com/apple/swift-numerics.git
-  PATCH_COMMAND ${swift-numerics_patch}
+  PATCH_COMMAND ${patch_swift_numerics}
   GIT_TAG        1.0.2
 )
