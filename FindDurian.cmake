6b98a54d8770e24cf1d69b19c81d93a5b7f4fc08
Dave Abrahams <dabrahams@adobe.com>
2024-04-01 18:32:21 -0700

    Add FindSwifty-LLVM and FindDurian

---
+++ FindDurian.cmake
@@ -0,0 +1,6 @@
+include(./FetchHyloDependency)
+
+fetch_hylo_dependency(Durian
+  GIT_REPOSITORY https://github.com/hylo-lang/Durian.git
+  GIT_TAG        1.2.1
+)
---
+++ FindSwifty-LLVM.cmake
@@ -0,0 +1,6 @@
+include(./FetchHyloDependency)
+
+fetch_hylo_dependency(Swifty-LLVM
+  GIT_REPOSITORY https://github.com/hylo-lang/Swifty-LLVM.git
+  GIT_TAG        main
+)
