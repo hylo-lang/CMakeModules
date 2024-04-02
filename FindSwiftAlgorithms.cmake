a1835ba61272f21f7029b8ac80d35e81fdf08a39
Dave Abrahams <dabrahams@adobe.com>
2024-04-02 15:48:54 -0700

    Deal with dependencies for SwiftAlgorithms

--- FindSwiftAlgorithms.cmake
+++ FindSwiftAlgorithms.cmake
@@ -1,6 +1,11 @@
+find_package(SwiftNumerics)
 include(./FetchHyloDependency)
 
 fetch_hylo_dependency(SwiftAlgorithms
   GIT_REPOSITORY https://github.com/apple/swift-algorithms.git
   GIT_TAG        1.2.0
 )
+
+set_recursive_file_glob(files ${swiftalgorithms_SOURCE_DIR}/Sources/*.swift)
+add_library(Algorithms ${files})
+target_link_libraries(Algorithms PRIVATE RealModule)
