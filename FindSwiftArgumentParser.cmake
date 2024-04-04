43ee6f599acdba43b038838608514eb9eb338e04
Dave Abrahams <dabrahams@adobe.com>
2024-04-04 14:41:44 -0700

    Work around Swift bug affecting CMake-generated Xcode projects
    https://github.com/apple/swift/issues/72841

--- FindSwiftArgumentParser.cmake
+++ FindSwiftArgumentParser.cmake
@@ -7,7 +7,13 @@ include(./FetchHyloDependency)
 list(PREPEND CMAKE_MODULE_PATH ${CMAKE_CURRENT_LIST_DIR})
 find_package(SwiftXCTest REQUIRED)
 
+set(patch_swift_argument_parser
+  ${CMAKE_COMMAND} -P ${CMAKE_CURRENT_LIST_DIR}/scripts/GitPatch.cmake
+  ${CMAKE_CURRENT_LIST_DIR}/patches/swift-argument-parser.patch)
+
 fetch_hylo_dependency(SwiftArgumentParser
   GIT_REPOSITORY https://github.com/apple/swift-argument-parser.git
   GIT_TAG        1.3.0
+  # Workaround for https://github.com/apple/swift/issues/72841
+  PATCH_COMMAND  ${patch_swift_argument_parser}
 )
