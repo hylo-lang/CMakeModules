61f5904a4068547561b844929e737a5a28d27614
Lucian Radu Teodorescu <lucteo@lucteo.ro>
2025-01-24 22:04:17 +0200

    Update Swifty-LLVM to commit "Ensure proper alignment for store instructions. (#83)"

--- FindSwifty-LLVM.cmake
+++ FindSwifty-LLVM.cmake
@@ -2,5 +2,5 @@ include(./FetchHyloDependency)
 
 fetch_hylo_dependency(Swifty-LLVM
   GIT_REPOSITORY https://github.com/hylo-lang/Swifty-LLVM.git
-  GIT_TAG        62cf26ece651a4aba4b6ba9dbbd29c7e69030501
+  GIT_TAG        509567462dfff80f93244884876dba27858453ef
 )
