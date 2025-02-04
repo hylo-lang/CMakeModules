ee6443e10a84ed11d49aafa8a9c4eeeb05f1aebe
Dave Abrahams <dabrahams@adobe.com>
2025-02-04 09:02:32 -0800

    Factoring hylo utilities

--- HyloUtilities.cmake
+++ HyloUtilities.cmake
@@ -33,3 +33,47 @@ function(set_recursive_file_glob variable)
     CONFIGURE_DEPENDS ${ARGN})
   set("${variable}" "${${variable}}" PARENT_SCOPE)
 endfunction()
+
+function(add_hylo_executable result_target)
+  cmake_parse_arguments("" # <prefix>
+    "" # <options>
+    "PATH" # <one_value_keywords>
+    "DEPENDENCIES" # <multi_value_keywords>
+    ${ARGN})
+  if(NOT _PATH)
+    set(_PATH ${result_target})
+  endif()
+  set_recursive_file_glob(files ${_PATH}/*.swift)
+  add_executable(${result_target} ${files})
+  target_depends(${result_target} ${_DEPENDENCIES})
+endfunction()
+
+function(add_hylo_library result_target)
+  cmake_parse_arguments("" # <prefix>
+    "" # <options>
+    "PATH" # <one_value_keywords>
+    "DEPENDENCIES" # <multi_value_keywords>
+    ${ARGN})
+  if(NOT _PATH)
+    set(_PATH ${result_target})
+  endif()
+  set_recursive_file_glob(files ${_PATH}/*.swift)
+  add_library(${result_target} ${files})
+  target_depends(${result_target} ${_DEPENDENCIES})
+endfunction()
+
+function(add_hylo_test_of testee)
+  cmake_parse_arguments("" # <prefix>
+    "" # <options>
+    "PATH;NAMED" # <one_value_keywords>
+    "DEPENDENCIES" # <multi_value_keywords>
+    ${ARGN})
+
+  set(result_target "${_NAMED}")
+  if(NOT _PATH)
+    set(_PATH ${result_target})
+  endif()
+  set_recursive_file_glob(files ${_PATH}/*.swift)
+  add_swift_xctest(${result_target} ${testee} ${files})
+  target_depends(${result_target} ${_DEPENDENCIES})
+endfunction()
