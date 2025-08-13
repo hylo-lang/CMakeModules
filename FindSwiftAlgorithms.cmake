642a6e173d4dbefaeb618a2946e209f6699e55d9
Ambrus Toth <ping@ambrus.dev>
2025-08-13 19:43:13 +0200

    Slim algorithms

--- FindSwiftAlgorithms.cmake
+++ FindSwiftAlgorithms.cmake
@@ -2,10 +2,9 @@ find_package(SwiftNumerics)
 include(./FetchHyloDependency)
 
 fetch_hylo_dependency(SwiftAlgorithms
-  GIT_REPOSITORY https://github.com/apple/swift-algorithms.git
-  GIT_TAG        1.2.1
+  GIT_REPOSITORY https://github.com/hylo-lang/swift-algorithms-slim
+  GIT_TAG        6fcdcfea3e8f349fcc1799cf2e6a68847b306e6d
 )
 
 set_recursive_file_glob(files ${swiftalgorithms_SOURCE_DIR}/Sources/*.swift)
 add_library(Algorithms ${files})
-target_link_libraries(Algorithms PRIVATE RealModule)
--- FindSwiftNumerics.cmake
+++ FindSwiftNumerics.cmake
@@ -1,8 +1,8 @@
 include(./FetchHyloDependency)
 
-# set(patch_swift_numerics
-#   ${CMAKE_COMMAND} -P ${CMAKE_CURRENT_LIST_DIR}/scripts/GitPatch.cmake
-#   ${CMAKE_CURRENT_LIST_DIR}/patches/swift-numerics.patch)
+set(patch_swift_numerics
+  ${CMAKE_COMMAND} -P ${CMAKE_CURRENT_LIST_DIR}/scripts/GitPatch.cmake
+  ${CMAKE_CURRENT_LIST_DIR}/patches/swift-numerics.patch)
 
 fetch_hylo_dependency(SwiftNumerics
   GIT_REPOSITORY https://github.com/tothambrus11/swift-numerics.git
