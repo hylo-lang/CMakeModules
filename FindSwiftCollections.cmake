c38597a0a10a0b86d90fd7ebd794aa1b17ca9634
Ambrus Toth <ping@ambrus.dev>
2025-08-13 21:00:06 +0200

    Use fork for SwiftCollections

--- FindSwiftCollections.cmake
+++ FindSwiftCollections.cmake
@@ -5,7 +5,6 @@ set(patch_swift_collections
   ${CMAKE_CURRENT_LIST_DIR}/patches/swift-collections.patch)
 
 fetch_hylo_dependency(SwiftCollections
-  GIT_REPOSITORY https://github.com/apple/swift-collections.git
-  PATCH_COMMAND ${patch_swift_collections}
-  GIT_TAG        1.0.6
+  GIT_REPOSITORY https://github.com/tothambrus11/swift-collections.git
+  GIT_TAG        965b519eceb31f6c1ce1408c5ee1a65745e2379f
 )
