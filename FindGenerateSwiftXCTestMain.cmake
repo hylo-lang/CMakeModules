ba628731c326c81ce28133459103125e5bc77a17
Ambrus Toth <ping@ambrus.dev>
2025-09-03 21:49:14 +0200

    Swift 6.1 support in GenerateSwiftXCTestMain

--- FindGenerateSwiftXCTestMain.cmake
+++ FindGenerateSwiftXCTestMain.cmake
@@ -4,4 +4,4 @@ include(./FetchHyloDependency)
 
 fetch_hylo_dependency(GenerateSwiftXCTestMain
   GIT_REPOSITORY https://github.com/hylo-lang/GenerateSwiftXCTestMain.git
-  GIT_TAG        main)
+  GIT_TAG        63645fca3779f927c62b6fff1202963672d8c4bc)
