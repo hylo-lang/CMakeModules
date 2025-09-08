2954e2d497a280b6d014c7e1a3241472d19bb004
Ambrus Toth <ping@ambrus.dev>
2025-09-08 22:22:54 +0200

    Update SwiftCollections

--- FindSwiftCollections.cmake
+++ FindSwiftCollections.cmake
@@ -6,5 +6,5 @@ set(patch_swift_collections
 
 fetch_hylo_dependency(SwiftCollections
   GIT_REPOSITORY https://github.com/hylo-lang/swift-collections
-  GIT_TAG        67e82c93a527081b0b3552a610e61e5bf7e0ad16
+  GIT_TAG        88ae722271e83dc3a22be45b36c7467a02c69292
 )
