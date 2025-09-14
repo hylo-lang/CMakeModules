b962939c7e95c2f24cf01eece8825096b46bf397
Ambrus Toth <ping@ambrus.dev>
2025-09-14 22:10:09 +0200

    Update Swifty-LLVM

--- FindSwifty-LLVM.cmake
+++ FindSwifty-LLVM.cmake
@@ -2,5 +2,5 @@ include(./FetchHyloDependency)
 
 fetch_hylo_dependency(Swifty-LLVM
   GIT_REPOSITORY https://github.com/hylo-lang/Swifty-LLVM.git
-  GIT_TAG        2835cc4f17a2a6e430407ddb6d01fd60355a8c71
+  GIT_TAG        cff484ecca159d9eec1938cda83a72b57cdfbd08
 )
