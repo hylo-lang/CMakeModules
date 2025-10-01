dfaceab08f9d6c4a8776d12db43c78a7cb455abc
Ambrus Toth <ping@ambrus.dev>
2025-10-01 19:25:31 +0200

    Remove Swift Numerics and dependency

--- FindSwiftAlgorithms.cmake
+++ FindSwiftAlgorithms.cmake
@@ -3,7 +3,7 @@ include(./FetchHyloDependency)
 
 fetch_hylo_dependency(SwiftAlgorithms
   GIT_REPOSITORY https://github.com/hylo-lang/swift-algorithms-slim
-  GIT_TAG        6fcdcfea3e8f349fcc1799cf2e6a68847b306e6d
+  GIT_TAG        e4d2507c1ce65dd4dd48a24979c9d0e0d02e8624
 )
 
 set_recursive_file_glob(files ${swiftalgorithms_SOURCE_DIR}/Sources/*.swift)
