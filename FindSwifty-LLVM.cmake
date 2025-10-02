698dfdcffb9d46071a3cefa29becb2074c76bb2c
Ambrus Toth <ping@ambrus.dev>
2025-10-02 20:29:15 +0200

    Upgrade Swifty-LLVM to support Xcode

--- FindSwifty-LLVM.cmake
+++ FindSwifty-LLVM.cmake
@@ -2,5 +2,5 @@ include(./FetchHyloDependency)
 
 fetch_hylo_dependency(Swifty-LLVM
   GIT_REPOSITORY https://github.com/hylo-lang/Swifty-LLVM.git
-  GIT_TAG        cff484ecca159d9eec1938cda83a72b57cdfbd08
+  GIT_TAG        5362acedda9b3b1198d1e4f54bf1a8c3e1fcacc5
 )
