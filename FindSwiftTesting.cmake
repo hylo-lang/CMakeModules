36adc08cccbd0056a06f82cc0bbaefc7c6938e75
Ambrus Toth <ping@ambrus.dev>
2025-11-01 18:54:00 +0100

    Find Swift testing

---
+++ FindSwiftTesting.cmake
@@ -0,0 +1,6 @@
+include(./FetchHyloDependency)
+
+fetch_hylo_dependency(SwiftTesting
+  GIT_REPOSITORY https://github.com/swiftlang/swift-testing.git
+  GIT_TAG        3fdabe5392108d874abae1c1e58e1328ab46f681
+)
