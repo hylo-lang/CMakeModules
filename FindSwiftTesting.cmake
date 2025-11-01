ed26b1260e67fed5059ce79b43d5b9da523187ff
Ambrus Toth <ping@ambrus.dev>
2025-11-01 19:56:22 +0100

    remove questionable cmake settings from swift testing

--- FindSwiftTesting.cmake
+++ FindSwiftTesting.cmake
@@ -1,6 +1,6 @@
 include(./FetchHyloDependency)
 
 fetch_hylo_dependency(SwiftTesting
-  GIT_REPOSITORY https://github.com/swiftlang/swift-testing.git
-  GIT_TAG        3fdabe5392108d874abae1c1e58e1328ab46f681
+  GIT_REPOSITORY https://github.com/hylo-lang/swift-testing.git
+  GIT_TAG        f6ea8ce2df06c77282c34bcb3f4752ca037ad168
 )
